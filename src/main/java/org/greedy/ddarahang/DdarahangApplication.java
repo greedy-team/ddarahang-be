b4dd69abda7c9b3f1b2dd05de3f150a153b84e9f
Shin Jihoon <tobeabestns@naver.com>
2025-02-11 17:16:56 +0900

    Initialize Project

---
+++ src/main/java/org/greedy/ddarahang/DdarahangApplication.java
@@ -0,0 +1,13 @@
+package org.greedy.ddarahang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdarahangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdarahangApplication.class, args);
+    }
+
+}
