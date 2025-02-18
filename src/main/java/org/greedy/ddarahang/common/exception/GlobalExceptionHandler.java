9ad1d63b7bcf7cf5de82edc212115246a99f9e88
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2025-02-18 20:15:08 +0900

    feature: exception 생성

---
+++ src/main/java/org/greedy/ddarahang/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package org.greedy.ddarahang.common.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler{
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String processValidationException(MethodArgumentNotValidException exception) {
+        BindingResult bindingResult = exception.getBindingResult();
+
+        StringBuilder builder = new StringBuilder();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            builder.append("[");
+            builder.append(fieldError.getField());
+            builder.append("](은)는 ");
+            builder.append(fieldError.getDefaultMessage());
+            builder.append(" 입력된 값: [");
+            builder.append(fieldError.getRejectedValue());
+            builder.append("]");
+        }
+
+        return builder.toString();
+    }
+
+    @ExceptionHandler(DdarahangException.class)
+    public ResponseEntity<String> handleExceptions(DdarahangException e) {
+        log.error("");
+        return ResponseEntity.internalServerError().body("프로그램 내 에러가 발생했습니다.");
+    }
+}
