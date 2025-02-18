92195e87c7ac5003f6dab77b1bfc6295f8448287
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2025-02-18 18:25:27 +0900

    feat: repository 생성

---
+++ src/main/java/org/greedy/ddarahang/db/region/RegionRepository.java
@@ -0,0 +1,6 @@
+package org.greedy.ddarahang.db.region;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionRepository extends JpaRepository<Region, Long> {
+}
