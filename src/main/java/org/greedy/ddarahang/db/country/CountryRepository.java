c3b98c12a94c3650421abaf7ee7f9512c5fa7325
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2025-02-18 20:13:01 +0900

    feature: repository 생성

--- src/main/java/org/greedy/ddarahang/db/country/CountryRepository.java
+++ src/main/java/org/greedy/ddarahang/db/country/CountryRepository.java
@@ -1,6 +1,8 @@
 package org.greedy.ddarahang.db.country;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CountryRepository extends JpaRepository<Country, Long> {
 }
