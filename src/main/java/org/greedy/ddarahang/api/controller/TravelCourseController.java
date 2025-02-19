0acc4e8f8ec1457ca21568f3094b68b6666740c6
Shin Jihoon <tobeabestns@naver.com>
2025-02-20 02:22:23 +0900

    refactor: @GetMapping의 파라미터 어노테이션을  @RequestBody -> @ModelAttribute로 변경

--- src/main/java/org/greedy/ddarahang/api/controller/TravelCourseController.java
+++ src/main/java/org/greedy/ddarahang/api/controller/TravelCourseController.java
@@ -5,10 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.greedy.ddarahang.api.dto.TravelCourseRequest;
 import org.greedy.ddarahang.api.dto.TravelCourseResponse;
 import org.greedy.ddarahang.api.service.TravelCourseService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,7 +17,7 @@ public class TravelCourseController {
     private final TravelCourseService travelCourseService;
 
     @GetMapping
-    public List<TravelCourseResponse> getTravelCourses(@RequestBody @Valid TravelCourseRequest travelCourseRequest) {
+    public List<TravelCourseResponse> getTravelCourses(@ModelAttribute @Valid TravelCourseRequest travelCourseRequest) {
         return travelCourseService.getTravelCourses(travelCourseRequest);
     }
 }
