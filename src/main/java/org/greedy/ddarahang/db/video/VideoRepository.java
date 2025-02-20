0cef36643486618678eb5cde5fa0225d407640e0
haeyoon1 <hynam2002@gmail.com>
2025-02-20 18:57:39 +0900

    style: EOL 수정

--- src/main/java/org/greedy/ddarahang/db/video/VideoRepository.java
+++ src/main/java/org/greedy/ddarahang/db/video/VideoRepository.java
@@ -1,6 +1,5 @@
 package org.greedy.ddarahang.db.video;
 
-import org.greedy.ddarahang.db.travelCourse.TravelCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
