ef935b754127a37d4150323944c4d97b0f828a5c
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2025-02-25 02:43:01 +0900

    feature: MissingIdException 커스텀 예외 클래스 추가

--- src/main/java/org/greedy/ddarahang/common/exception/GlobalExceptionHandler.java
+++ src/main/java/org/greedy/ddarahang/common/exception/GlobalExceptionHandler.java
@@ -2,33 +2,12 @@ package org.greedy.ddarahang.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler{
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public String processValidationException(MethodArgumentNotValidException exception) {
-        BindingResult bindingResult = exception.getBindingResult();
-
-        StringBuilder builder = new StringBuilder();
-        for (FieldError fieldError : bindingResult.getFieldErrors()) {
-            builder.append("[");
-            builder.append(fieldError.getField());
-            builder.append("](은)는 ");
-            builder.append(fieldError.getDefaultMessage());
-            builder.append(" 입력된 값: [");
-            builder.append(fieldError.getRejectedValue());
-            builder.append("]");
-        }
-
-        return builder.toString();
-    }
 
     @ExceptionHandler(NotFoundTravelCourseDetailException.class)
     public ResponseEntity<String> notFoundTravelCourseDetailException(NotFoundTravelCourseDetailException e) {
@@ -38,6 +17,12 @@ public class GlobalExceptionHandler{
 
     @ExceptionHandler(InvalidCountryNameException.class)
     public ResponseEntity<String> invalidCountryNameException(InvalidCountryNameException e) {
+        log.error("");
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(MissingIdException.class)
+    public ResponseEntity<String> missingIdException(MissingIdException e) {
         log.error("");
         return ResponseEntity.badRequest().body(e.getMessage());
     }
