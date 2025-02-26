333e0984dcb476e23d8aa9538128739d604f6d60
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2025-02-27 01:52:12 +0900

    feat: fixture 데이터 추가

--- src/test/java/org/greedy/ddarahang/common/fixture/PlaceFixture.java
+++ src/test/java/org/greedy/ddarahang/common/fixture/PlaceFixture.java
@@ -4,10 +4,18 @@ import org.greedy.ddarahang.db.place.Place;
 import org.greedy.ddarahang.db.region.Region;
 
 public class PlaceFixture {
-    public static Place getMockPlace(Region region) {
+    public static Place getMockPlace_1(Region region) {
         return Place.builder()
                 .name("경복궁")
                 .address("서울 종로구 세종로 1-1")
+                .region(region)
+                .build();
+    }
+
+    public static Place getMockPlace_2(Region region) {
+        return Place.builder()
+                .name("광안리 해수욕장")
+                .address("부산 광안리")
                 .region(region)
                 .build();
     }
