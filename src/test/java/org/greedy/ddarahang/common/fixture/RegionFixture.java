068506f9adf943e6feb72070f05c6ef0d936b41d
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2025-02-27 01:52:12 +0900

    feat: fixture 데이터 추가

--- src/test/java/org/greedy/ddarahang/common/fixture/RegionFixture.java
+++ src/test/java/org/greedy/ddarahang/common/fixture/RegionFixture.java
@@ -4,9 +4,16 @@ import org.greedy.ddarahang.db.country.Country;
 import org.greedy.ddarahang.db.region.Region;
 
 public class RegionFixture {
-    public static Region getMockRegion(Country country) {
+    public static Region getMockRegion_1(Country country) {
         return Region.builder()
                 .name("서울")
+                .country(country)
+                .build();
+    }
+
+    public static Region getMockRegion_2(Country country) {
+        return Region.builder()
+                .name("부산")
                 .country(country)
                 .build();
     }
--- src/test/java/org/greedy/ddarahang/common/fixture/VideoFixture.java
+++ src/test/java/org/greedy/ddarahang/common/fixture/VideoFixture.java
@@ -5,13 +5,24 @@ import org.greedy.ddarahang.db.video.Video;
 import java.time.LocalDate;
 
 public class VideoFixture {
-    public static Video getMockVideo(LocalDate now) {
+    public static Video getMockVideo_1(LocalDate now) {
         return Video.builder()
                 .creator("여행유튜버A")
                 .title("서울 여행 브이로그")
                 .videoUrl("https://youtube.com/v1")
                 .thumbnailUrl("https://img.youtube.com/1.jpg")
                 .viewCount(10000L)
+                .uploadDate(now)
+                .build();
+    }
+
+    public static Video getMockVideo_2(LocalDate now) {
+        return Video.builder()
+                .creator("여행유튜버B")
+                .title("부산 여행 브이로그")
+                .videoUrl("https://youtube.com/v2")
+                .thumbnailUrl("https://img.youtube.com/2.jpg")
+                .viewCount(20000L)
                 .uploadDate(now)
                 .build();
     }
