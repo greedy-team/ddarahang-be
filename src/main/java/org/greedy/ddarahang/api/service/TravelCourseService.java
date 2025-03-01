c87061660075f281e80976838e5891592c068ebd
Shin Jihoon <tobeabestns@naver.com>
2025-03-02 00:09:12 +0900

    feat: service 로그 전략 적용

--- src/main/java/org/greedy/ddarahang/api/service/TravelCourseService.java
+++ src/main/java/org/greedy/ddarahang/api/service/TravelCourseService.java
@@ -1,6 +1,7 @@
 package org.greedy.ddarahang.api.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.greedy.ddarahang.api.dto.TravelCourseDetailResponse;
 import org.greedy.ddarahang.api.dto.TravelCourseListResponse;
 import org.greedy.ddarahang.api.dto.TravelCourseResponse;
@@ -15,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -25,6 +27,7 @@ public class TravelCourseService {
 
     public List<TravelCourseListResponse> getTravelCourses(String filter, String countryName, String regionName) {
         validateCountryName(countryName);
+        log.info("여행 코스 목록 조회: filter: {}, countryName: {}, regionName: {}", filter, countryName, regionName);
 
         if (filter.equalsIgnoreCase("default")) {
             if (regionName.isBlank()) {
@@ -59,14 +62,19 @@ public class TravelCourseService {
                     .toList();
         }
 
+        log.warn("유효하지 않은 필터값: filter: {}", filter);
         throw new IllegalArgumentException("Invalid filter value : " + filter);
     }
 
     public TravelCourseResponse getTravelCourseDetail(Long id) {
         validateId(id);
+        log.info("여행 목록 상세 조회: ID: {}", id);
 
         TravelCourse travelCourse = travelCourseRepository.findById(id)
-                .orElseThrow(() -> new NotFoundTravelCourseDetailException("travel course not found"));
+                .orElseThrow(() -> {
+                    log.error("해당 id를 가진 여행 상세 정보가 존재하지 않습니다. ID: {}", id);
+                    return new NotFoundTravelCourseDetailException("travel course not found");
+                });
 
         List<TravelCourseDetailResponse> travelCourseDetails = travelCourseDetailRepository.findAllByTravelCourseId(id)
                 .stream().map(TravelCourseDetailResponse::from).toList();
@@ -76,12 +84,14 @@ public class TravelCourseService {
 
     private void validateCountryName(String countryName) {
         if (countryName == null || countryName.isBlank()) {
+            log.warn("유효하지 않은 지역 이름입니다. countryName: {}", countryName);
             throw new InvalidCountryNameException("invalid country name");
         }
     }
 
     private void validateId(Long id) {
         if (id == null) {
+            log.warn("유효하지 않은 Id값 입니다. id: null");
             throw new MissingIdException("invalid id");
         }
     }
