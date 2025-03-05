ff17940dd6901f905548e45199b3656974af4dbb
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2025-03-05 17:46:21 +0900

    feat: custom exception 추가

---
+++ src/main/java/org/greedy/ddarahang/common/exception/InvalidDateFormat.java
@@ -0,0 +1,7 @@
+package org.greedy.ddarahang.common.exception;
+
+public class InvalidDateFormat extends RuntimeException {
+    public InvalidDateFormat(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/greedy/ddarahang/common/exception/NotFoundTravelCourseException.java
@@ -0,0 +1,7 @@
+package org.greedy.ddarahang.common.exception;
+
+public class NotFoundTravelCourseException extends RuntimeException{
+    public NotFoundTravelCourseException(String message) {
+        super(message);
+    }
+}
