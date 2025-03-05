d4bf39c307e910eef0a697649c3c7e5d40b5f4fd
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2025-03-05 17:46:21 +0900

    feat: custom exception 추가

---
+++ src/main/java/org/greedy/ddarahang/common/exception/NotFoundPlaceException.java
@@ -0,0 +1,7 @@
+package org.greedy.ddarahang.common.exception;
+
+public class NotFoundPlaceException extends RuntimeException {
+    public NotFoundPlaceException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/greedy/ddarahang/common/exception/NotFoundVideoException.java
@@ -0,0 +1,7 @@
+package org.greedy.ddarahang.common.exception;
+
+public class NotFoundVideoException extends RuntimeException {
+    public NotFoundVideoException(String message) {
+        super(message);
+    }
+}
