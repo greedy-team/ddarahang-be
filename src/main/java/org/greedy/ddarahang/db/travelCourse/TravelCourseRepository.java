a54463d6edfc05f4c6d757115669eae11fbb718d
Shin Jihoon <tobeabestns@naver.com>
2025-03-09 02:57:41 +0900

    refactor: travelCourse 조회 시 발생하는 N+1 문제 해결

--- src/main/java/org/greedy/ddarahang/db/travelCourse/TravelCourseRepository.java
+++ src/main/java/org/greedy/ddarahang/db/travelCourse/TravelCourseRepository.java
@@ -1,23 +1,36 @@
 package org.greedy.ddarahang.db.travelCourse;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TravelCourseRepository extends JpaRepository<TravelCourse, Long> {
 
+    @EntityGraph(attributePaths = {"video"})
+    @NonNull
+    Optional<TravelCourse> findById(@NonNull Long id);
+
+    @EntityGraph(attributePaths = {"video"})
     List<TravelCourse> findAllByCountryName(String countryName);
 
+    @EntityGraph(attributePaths = {"video"})
     List<TravelCourse> findAllByRegionName(String regionName);
 
+    @EntityGraph(attributePaths = {"video"})
     List<TravelCourse> findAllByCountryNameOrderByVideoUploadDateDesc(String countryName);
 
+    @EntityGraph(attributePaths = {"video"})
     List<TravelCourse> findAllByRegionNameOrderByVideoUploadDateDesc(String regionName);
 
+    @EntityGraph(attributePaths = {"video"})
     List<TravelCourse> findAllByCountryNameOrderByVideoViewCountDesc(String countryName);
 
+    @EntityGraph(attributePaths = {"video"})
     List<TravelCourse> findAllByRegionNameOrderByVideoViewCountDesc(String regionName);
 
 }
