f166df67e73a7a39ba97496c310b3529260ea20c
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2025-03-11 16:41:09 +0900

    feat: DataSyncException 예외처리

---
+++ src/main/java/org/greedy/ddarahang/common/exception/DataSyncException.java
@@ -0,0 +1,7 @@
+package org.greedy.ddarahang.common.exception;
+
+public class DataSyncException extends RuntimeException {
+    public DataSyncException(String message) {
+        super(message);
+    }
+}
