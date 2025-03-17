d7b59c4a538c248bb6440ca245f0c637fb631d3c
gogo1414 <wnstn6945@gmail.com>
2025-03-18 02:49:05 +0900

    chore: test 환경과 운영환경의 설정 분리

---
+++ src/main/java/org/greedy/ddarahang/api/controller/GoogleSheetController.java
@@ -0,0 +1,24 @@
+package org.greedy.ddarahang.api.controller;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import lombok.RequiredArgsConstructor;
+import org.greedy.ddarahang.api.service.TravelDataSyncService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/data")
+@RequiredArgsConstructor
+public class GoogleSheetController {
+
+    private final TravelDataSyncService travelDataSyncService;
+
+    @PostMapping
+    public ResponseEntity<Void> syncGoogleSheetWithDB() throws GeneralSecurityException, IOException {
+        travelDataSyncService.syncGoogleSheetWithDB();
+        return ResponseEntity.ok().build();
+    }
+}
