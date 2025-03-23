6b70a2fbdb1495da40de117fd2143047007e001b
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2025-03-24 02:47:16 +0900

    fix: get_current_port 반환값 및 cp 명령 권한 오류 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,9 +7,9 @@ timestamp=$(date +"%Y%m%d%H%M%S")
 
 get_current_port() {
     if sudo fuser $BLUE_PORT/tcp >/dev/null 2>&1 && ! sudo fuser $GREEN_PORT/tcp >/dev/null 2>&1; then
-        echo "BLUE_PORT: $BLUE_PORT"
+        echo "$BLUE_PORT"
     elif sudo fuser $GREEN_PORT/tcp >/dev/null 2>&1 && ! sudo fuser $BLUE_PORT/tcp >/dev/null 2>&1; then
-        echo "GREEN_PORT: $GREEN_PORT"
+        echo "$GREEN_PORT"
     else
         echo "none"
     fi
@@ -19,8 +19,6 @@ health_check() {
     local check_url="http://localhost:$1/actuator/health"
     local retry_count=0
     local max_retry=10
-
-    echo "$1 포트에서 헬스 체크 중..."
 
     while [ $retry_count -lt $max_retry ]; do
         if curl --silent --head "$check_url" >/dev/null 2>&1; then
@@ -40,8 +38,6 @@ rollback() {
     local port=$1
     local backup_jar="/home/ubuntu/ddarahang/old_build/app-${port}-${timestamp}.jar"
     local current_version_jar="/home/ubuntu/ddarahang/app-${port}.jar"
-
-    echo "$port 포트에서 롤백 중..."
 
     sudo fuser -k -TERM $port/tcp
     if [ -f "$backup_jar" ]; then
@@ -65,8 +61,6 @@ deploy() {
     local current_version_jar="$app_dir/app-${port}.jar"
     local new_version_jar="$app_dir/build/libs/ddarahang.jar"
     local backup_dir="$app_dir/old_build"
-
-    echo "$port 포트에 새 버전 배포 중..."
 
     if [ ! -f "$new_version_jar" ]; then
         echo "오류: $new_version_jar 파일을 찾을 수 없습니다. 배포를 중단합니다..."
@@ -98,8 +92,7 @@ switch_traffic() {
     local config_file="/etc/nginx/sites-available/default"
     local new_proxy_pass="proxy_pass http://127.0.0.1:$2;"
 
-    echo "Nginx 트래픽을 $2 포트로 전환합니다..."
-    cp "$config_file" "$config_file.bak"
+    sudo cp "$config_file" "$config_file.bak"
 
     sudo sed -i '/location \/ {/!b;n;s|proxy_pass http://127.0.0.1:.*;|'"$new_proxy_pass"'|' "$config_file"
     if [ $? -ne 0 ]; then
@@ -112,20 +105,17 @@ switch_traffic() {
 current_port=$(get_current_port)
 if [ "$current_port" != "none" ]; then
     if [ "$current_port" -eq "$BLUE_PORT" ]; then
-        echo "$BLUE_PORT 포트가 실행 중, $GREEN_PORT 포트로 배포 중..."
         deploy "$GREEN_PORT" || exit 1
         switch_traffic "$BLUE_PORT" "$GREEN_PORT"
         sudo fuser -k -TERM "$BLUE_PORT"/tcp
         echo "Blue-Green 배포 완료 : $GREEN_PORT 포트에서만 실행 중"
     else
-        echo "$GREEN_PORT 포트가 실행 중, $BLUE_PORT 포트로 배포 중..."
         deploy "$BLUE_PORT" || exit 1
         switch_traffic "$GREEN_PORT" "$BLUE_PORT"
         sudo fuser -k -TERM "$GREEN_PORT"/tcp
         echo "Blue-Green 배포 완료 : $BLUE_PORT 포트에서만 실행 중"
     fi
 else
-    echo "실행 중인 포트가 없거나 두 포트 모두 활성화 되어 있습니다. $BLUE_PORT 포트에서 배포 시작..."
     deploy "$BLUE_PORT" || exit 1
     switch_traffic "$GREEN_PORT" "$BLUE_PORT"
     echo "Blue-Green 배포 완료 : $BLUE_PORT 포트에서만 실행 중"
