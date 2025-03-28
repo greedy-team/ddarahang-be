a0201008d39b0f06fa2dc7cb757158c02278cebe
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2025-03-29 04:43:36 +0900

    feat: getTravelCourses의 반환을 페이징 객체로 반환하도록 로직 수정

--- src/main/java/org/greedy/ddarahang/api/service/TravelCourseService.java
+++ src/main/java/org/greedy/ddarahang/api/service/TravelCourseService.java
@@ -3,15 +3,17 @@ package org.greedy.ddarahang.api.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.greedy.ddarahang.api.dto.TravelCourseDetailResponse;
+import org.greedy.ddarahang.api.dto.TravelCourseListRequest;
 import org.greedy.ddarahang.api.dto.TravelCourseListResponse;
 import org.greedy.ddarahang.api.dto.TravelCourseResponse;
-import org.greedy.ddarahang.common.exception.InvalidCountryNameException;
-import org.greedy.ddarahang.common.exception.InvalidFilterException;
 import org.greedy.ddarahang.common.exception.MissingIdException;
 import org.greedy.ddarahang.common.exception.NotFoundTravelCourseDetailException;
 import org.greedy.ddarahang.db.travelCourse.TravelCourse;
 import org.greedy.ddarahang.db.travelCourse.TravelCourseRepository;
 import org.greedy.ddarahang.db.travelCourseDetail.TravelCourseDetailRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,50 +28,35 @@ public class TravelCourseService {
     private final TravelCourseRepository travelCourseRepository;
     private final TravelCourseDetailRepository travelCourseDetailRepository;
 
-    public List<TravelCourseListResponse> getTravelCourses(String filter, String countryName, String regionName) {
-        validateCountryName(countryName);
-        log.info("여행 코스 목록 조회: filter: {}, countryName: {}, regionName: {}", filter, countryName, regionName);
+    public Page<TravelCourseListResponse> getTravelCourses(TravelCourseListRequest request) {
+        Pageable pageable = PageRequest.of(request.pageNumber(), request.pageSize());
 
-        if (filter.equalsIgnoreCase("default")) {
-            if (regionName.isBlank()) {
-                return travelCourseRepository.findAllByCountryName(countryName)
-                        .stream().map(travelCourse -> TravelCourseListResponse.from(travelCourse, travelCourse.getVideo()))
-                        .toList();
-            }
-            return travelCourseRepository.findAllByRegionName(regionName)
-                    .stream().map(travelCourse -> TravelCourseListResponse.from(travelCourse, travelCourse.getVideo()))
-                    .toList();
+        Page<TravelCourse> travelCourses;
+        if ("viewCount".equals(request.sortField())) {
+            travelCourses = travelCourseRepository.findAllByViewCount(
+                    request.countryName(), request.regionName(), pageable
+            );
+        }
+        else if ("uploadDate".equals(request.sortField())) {
+            travelCourses = travelCourseRepository.findAllByUploadDate(
+                    request.countryName(), request.regionName(), pageable
+            );
+        }
+        else {
+            travelCourses = travelCourseRepository.findAllByOrderByIdDesc(
+                    request.countryName(), request.regionName(), pageable
+            );
         }
 
-        if (filter.equalsIgnoreCase("viewCount")) {
-            if (regionName.isBlank()) {
-                return travelCourseRepository.findAllByCountryNameOrderByVideoViewCountDesc(countryName)
-                        .stream().map(travelCourse -> TravelCourseListResponse.from(travelCourse, travelCourse.getVideo()))
-                        .toList();
-            }
-            return travelCourseRepository.findAllByRegionNameOrderByVideoViewCountDesc(regionName)
-                    .stream().map(travelCourse -> TravelCourseListResponse.from(travelCourse, travelCourse.getVideo()))
-                    .toList();
-        }
+        log.info("여행 목록 정렬 성공: {}", request.sortField());
 
-        if (filter.equalsIgnoreCase("uploadDate")) {
-            if (regionName.isBlank()) {
-                return travelCourseRepository.findAllByCountryNameOrderByVideoUploadDateDesc(countryName)
-                        .stream().map(travelCourse -> TravelCourseListResponse.from(travelCourse, travelCourse.getVideo()))
-                        .toList();
-            }
-            return travelCourseRepository.findAllByRegionNameOrderByVideoUploadDateDesc(regionName)
-                    .stream().map(travelCourse -> TravelCourseListResponse.from(travelCourse, travelCourse.getVideo()))
-                    .toList();
-        }
-
-        log.error("유효하지 않은 필터값: filter: {}", filter);
-        throw new InvalidFilterException("Invalid filter value : " + filter);
+        return travelCourses.map(travelCourse ->
+                TravelCourseListResponse.from(travelCourse, travelCourse.getVideo())
+        );
     }
 
     public TravelCourseResponse getTravelCourseDetail(Long id) {
         validateId(id);
-        log.info("여행 목록 상세 조회: ID: {}", id);
 
         TravelCourse travelCourse = travelCourseRepository.findById(id)
                 .orElseThrow(() -> {
@@ -80,17 +67,12 @@ public class TravelCourseService {
         List<TravelCourseDetailResponse> travelCourseDetails = travelCourseDetailRepository.findAllByTravelCourseId(id)
                 .stream().map(TravelCourseDetailResponse::from).toList();
 
-        return TravelCourseResponse.from(travelCourse.getTravelDays(), travelCourse.getVideo(), travelCourseDetails);
-    }
-
-    private void validateCountryName(String countryName) {
-        if (countryName == null || countryName.isBlank()) {
-            log.error("유효하지 않은 지역 이름입니다. countryName: {}", countryName);
-            throw new InvalidCountryNameException("invalid country name");
-        }
+        return TravelCourseResponse.from(travelCourse, travelCourseDetails);
     }
 
     private void validateId(Long id) {
+        log.info("여행 목록 상세 조회: ID: {}", id);
+
         if (id == null) {
             log.error("유효하지 않은 Id값 입니다. id: null");
             throw new MissingIdException("invalid id");
