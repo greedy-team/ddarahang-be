cffc84259c1e56c4027faf2b8417a4bb5d59bb45
SANGHEEJEONG <147232817+SANGHEEJEONG@users.noreply.github.com>
2025-03-30 22:21:52 +0900

    refactor: pageSize 필드 제거 및 테스트 코드 수정

--- src/main/java/org/greedy/ddarahang/api/dto/TravelCourseListRequest.java
+++ src/main/java/org/greedy/ddarahang/api/dto/TravelCourseListRequest.java
@@ -17,22 +17,13 @@ public record TravelCourseListRequest(
         @Max(value = Integer.MAX_VALUE, message = "페이지 번호의 최대값을 초과했습니다.")
         Integer pageNumber,
 
-        @Min(value = 0, message = "페이지 사이즈에 음수는 입력될 수 없습니다.")
-        @Max(value = Integer.MAX_VALUE, message = "페이지 사이즈의 최대값을 초과했습니다.")
-        Integer pageSize,
-
         @Pattern(regexp = "^(viewCount|uploadDate|)")
         String sortField
 ) {
-
-        private static final int DEFAULT_PAGE_SIZE = 8;
-
         public TravelCourseListRequest {
                 if (pageNumber == null) {
                         pageNumber = 0;
                 }
-
-                pageSize = DEFAULT_PAGE_SIZE;
 
                 if (sortField == null || sortField.isBlank()) {
                         sortField = "uploadDate";
