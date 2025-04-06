a0466d7fa6f81d469bcd9e8ccca946adf01ea342
Shin Jihoon <tobeabestns@naver.com>
2025-04-07 03:21:44 +0900

    feat: 찜 목록 전체 조회 기능 추가

--- src/main/java/org/greedy/ddarahang/api/controller/FavoriteController.java
+++ src/main/java/org/greedy/ddarahang/api/controller/FavoriteController.java
@@ -3,10 +3,12 @@ package org.greedy.ddarahang.api.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.greedy.ddarahang.api.dto.favoriteDTO.CreateFavoriteListRequest;
-import org.greedy.ddarahang.api.dto.favoriteDTO.CreateFavoriteListResponse;
+import org.greedy.ddarahang.api.dto.favoriteDTO.FavoriteListResponse;
 import org.greedy.ddarahang.api.service.FavoriteListService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -17,7 +19,12 @@ public class FavoriteController {
     private final FavoriteListService favoriteListService;
 
     @PostMapping("/list")
-    public ResponseEntity<CreateFavoriteListResponse> createFavoriteList(@RequestBody CreateFavoriteListRequest request) {
+    public ResponseEntity<FavoriteListResponse> createFavoriteList(@RequestBody CreateFavoriteListRequest request) {
         return ResponseEntity.ok(favoriteListService.createFavoriteListResponse(request.listName()));
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<FavoriteListResponse>> getFavoriteLists() {
+        return ResponseEntity.ok(favoriteListService.getFavoriteLists());
     }
 }
--- src/main/java/org/greedy/ddarahang/api/service/FavoriteListService.java
+++ src/main/java/org/greedy/ddarahang/api/service/FavoriteListService.java
@@ -2,11 +2,13 @@ package org.greedy.ddarahang.api.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.greedy.ddarahang.api.dto.favoriteDTO.CreateFavoriteListResponse;
+import org.greedy.ddarahang.api.dto.favoriteDTO.FavoriteListResponse;
 import org.greedy.ddarahang.db.favoriteList.FavoriteList;
 import org.greedy.ddarahang.db.favoriteList.FavoriteListRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -17,11 +19,17 @@ public class FavoriteListService {
     private final FavoriteListRepository favoriteListRepository;
 
     @Transactional
-    public CreateFavoriteListResponse createFavoriteListResponse(String listName) {
+    public FavoriteListResponse createFavoriteListResponse(String listName) {
         FavoriteList favoriteList = FavoriteList.builder()
                 .listName(listName)
                 .build();
         FavoriteList saved = favoriteListRepository.save(favoriteList);
-        return CreateFavoriteListResponse.from(saved);
+        return FavoriteListResponse.from(saved);
+    }
+
+    public List<FavoriteListResponse> getFavoriteLists() {
+        return favoriteListRepository.findAll()
+                .stream().map(FavoriteListResponse::from)
+                .toList();
     }
 }
