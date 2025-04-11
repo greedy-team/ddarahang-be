3faa25ce3a01c1c0c9a9e12fbb5029015b23921f
Shin Jihoon <tobeabestns@naver.com>
2025-04-11 15:27:53 +0900

    refactor: @Repository 추가

--- src/main/java/org/greedy/ddarahang/db/favoriteList/FavoriteListPlaceRepository.java
+++ src/main/java/org/greedy/ddarahang/db/favoriteList/FavoriteListPlaceRepository.java
@@ -1,6 +1,8 @@
 package org.greedy.ddarahang.db.favoriteList;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FavoriteListPlaceRepository extends JpaRepository<FavoriteListPlace, Long> {
 }
