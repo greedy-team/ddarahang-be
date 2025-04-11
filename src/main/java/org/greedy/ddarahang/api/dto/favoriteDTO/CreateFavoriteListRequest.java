271e6d9d046cf2e3581c21723de8b3ab31cc1a05
Shin Jihoon <tobeabestns@naver.com>
2025-04-11 15:01:58 +0900

    refactor: @Valid 사용하여 요청에 대한 유효성 검사 추가

--- src/main/java/org/greedy/ddarahang/api/dto/favoriteDTO/CreateFavoriteListRequest.java
+++ src/main/java/org/greedy/ddarahang/api/dto/favoriteDTO/CreateFavoriteListRequest.java
@@ -1,6 +1,10 @@
 package org.greedy.ddarahang.api.dto.favoriteDTO;
 
+import jakarta.validation.constraints.NotNull;
+
 public record CreateFavoriteListRequest(
+
+        @NotNull(message = "찜 목록명은 필수입니다.")
         String listName,
         String description
 ) {
