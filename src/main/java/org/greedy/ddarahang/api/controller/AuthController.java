974d42ee52f6c2253a040ac912b7cd71b3472167
haeyoon1 <hynam1220@naver.com>
2025-05-04 14:16:24 +0900

    feat: 로그인, 회원가입 controller 생성

--- src/main/java/org/greedy/ddarahang/api/controller/AuthController.java
+++ src/main/java/org/greedy/ddarahang/api/controller/AuthController.java
@@ -1,12 +1,34 @@
 package org.greedy.ddarahang.api.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.greedy.ddarahang.api.dto.SignInRequest;
+import org.greedy.ddarahang.api.dto.SignUpRequest;
+import org.greedy.ddarahang.api.dto.TokenResponse;
 import org.greedy.ddarahang.api.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
 public class AuthController {
 
     private final AuthService authService;
+
+    @PostMapping("/signUp")
+    public ResponseEntity<TokenResponse> signUp(@RequestBody SignUpRequest request) {
+        if (authService.findByNickname(request.nickname()).isPresent()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.signUp(request));
+    }
+
+    @PostMapping("/signIn")
+    public ResponseEntity<TokenResponse> signIn(@RequestBody SignInRequest request) {
+        return ResponseEntity.status(HttpStatus.OK).body(authService.signIn(request));
+    }
 }
