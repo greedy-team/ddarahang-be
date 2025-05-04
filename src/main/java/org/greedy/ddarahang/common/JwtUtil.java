312c237f1e462a15e70850a6fcbf6718858e6bd0
haeyoon1 <hynam1220@naver.com>
2025-05-04 14:17:58 +0900

    feat: jwt 생성 및 검증 파일 생성

---
+++ src/main/java/org/greedy/ddarahang/common/JwtUtil.java
@@ -0,0 +1,64 @@
+package org.greedy.ddarahang.common;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import org.greedy.ddarahang.db.user.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.access-token-expiration}")
+    private Long accessTokenExpiration;
+    @Value("${jwt.refresh-token-expiration}")
+    private Long refreshTokenExpiration;
+
+    private final SecretKey secretKey;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        this.secretKey = new SecretKeySpec(
+                secret.getBytes(StandardCharsets.UTF_8),
+                "HmacSHA256");
+    }
+
+    public String createAccessToken(User user) {
+        return Jwts.builder()
+                .claim("nickname", user.getNickname())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + accessTokenExpiration))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String createRefreshToken(User user) {
+        return Jwts.builder()
+                .claim("nickname", user.getNickname())
+                .expiration(new Date(System.currentTimeMillis() + refreshTokenExpiration))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public boolean isValidRefreshToken(String refreshToken) {
+        try {
+            getClaimsToken(refreshToken);
+            return true;
+        } catch (NullPointerException | JwtException e) {
+            return false;
+        }
+    }
+
+    public Claims getClaimsToken(String refreshToken) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(refreshToken)
+                .getPayload();
+    }
+}
