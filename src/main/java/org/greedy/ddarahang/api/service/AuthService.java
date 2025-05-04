285bf10d5776d869da908db8993666343b97cdef
haeyoon1 <hynam1220@naver.com>
2025-05-04 14:16:31 +0900

    feat: 로그인, 회원가입 service 생성

--- src/main/java/org/greedy/ddarahang/api/service/AuthService.java
+++ src/main/java/org/greedy/ddarahang/api/service/AuthService.java
@@ -1,7 +1,88 @@
 package org.greedy.ddarahang.api.service;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.greedy.ddarahang.api.dto.SignInRequest;
+import org.greedy.ddarahang.api.dto.SignUpRequest;
+import org.greedy.ddarahang.api.dto.TokenResponse;
+import org.greedy.ddarahang.common.JwtUtil;
+import org.greedy.ddarahang.db.token.RefreshToken;
+import org.greedy.ddarahang.db.token.RefreshTokenRepository;
+import org.greedy.ddarahang.db.user.User;
+import org.greedy.ddarahang.db.user.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final JwtUtil jwtUtil;
+
+    private final UserRepository userRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Transactional
+    public TokenResponse signUp(SignUpRequest request) {
+        User newUser = User.builder()
+                .nickname(request.nickname())
+                .password(request.password()) //.password(new BCryptPasswordEncoder().encode(request.password()))
+                .email(request.email())
+                .build();
+
+        String accessToken = jwtUtil.createAccessToken(newUser);
+        String refreshToken = jwtUtil.createRefreshToken(newUser);
+
+        userRepository.save(newUser);
+        refreshTokenRepository.save(
+                RefreshToken.builder()
+                        .user(newUser)
+                        .token(refreshToken)
+                        .build()
+        );
+
+        return TokenResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    @Transactional
+    public TokenResponse signIn(SignInRequest request) {
+
+        User user = userRepository.findByNickname(request.nickname())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+
+        if (!request.password().equals(user.getPassword())) {
+            log.info("비밀번호가 일치하지 않습니다! request_password: " + request.password() + ", user_password: " + user.getPassword());
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        RefreshToken refreshTokenEntity = refreshTokenRepository.findByUserId(user.getId())
+                .orElseThrow(()-> new IllegalArgumentException("토큰이 존재하지 않습니다,"));
+
+        String accessToken = "";
+        String refreshToken = refreshTokenEntity.getToken();
+        if (jwtUtil.isValidRefreshToken(refreshToken)) {
+            accessToken = jwtUtil.createAccessToken(user);
+            return TokenResponse.builder()
+                    .accessToken(accessToken)
+                    .refreshToken(refreshToken)
+                    .build();
+        } else {
+            refreshToken = jwtUtil.createRefreshToken(user);
+            refreshTokenEntity.updateToken(refreshToken);
+        }
+        return TokenResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    public Optional<User> findByNickname(String nickname) {
+        return userRepository.findByNickname(nickname);
+    }
 }
