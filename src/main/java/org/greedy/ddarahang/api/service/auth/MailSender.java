f09f268ac3fc6a02a8f4a68212a99c846d9a1673
haeyoon1 <hynam1220@naver.com>
2025-05-06 02:14:45 +0900

    feat: 메일 전송 및 인증 service 구현

---
+++ src/main/java/org/greedy/ddarahang/api/service/auth/MailSender.java
@@ -0,0 +1,33 @@
+package org.greedy.ddarahang.api.service.auth;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MailSender {
+
+    private final JavaMailSender emailSender;
+
+    public void sendMail(String to, String title, String content) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setTo(to);
+        helper.setSubject(title);
+        helper.setText(content, true);
+        helper.setReplyTo("ddarahang@gmail.com");
+        try{
+            emailSender.send(message);
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Unable to send email", e);
+        }
+    }
+}
---
+++ src/main/java/org/greedy/ddarahang/api/service/auth/MailService.java
@@ -0,0 +1,64 @@
+package org.greedy.ddarahang.api.service.auth;
+
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.greedy.ddarahang.db.token.VerificationCode;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+    private final MailSender mailSender;
+
+    private final Map<String, VerificationCode> codeMap = new ConcurrentHashMap<>();
+
+    public void sendCodeToEmail(String email) { //이메일 전송
+        String code = generateRandomCode(6);
+        LocalDateTime expireTime = LocalDateTime.now().plusMinutes(10);
+
+        codeMap.put(email, new VerificationCode(code, expireTime));
+
+        String title = "DDARAHANG(따라행) 이메일 인증 번호";
+        String content = "<html>"
+                + "<body>"
+                + "<h1>ddaragang(따라행) 인증 코드" + code + "</h1>"
+                + "<p>10분 안에 입력해주세요.</p>"
+                + "</body>"
+                + "</html>";
+        try {
+            mailSender.sendMail(email, title, content);
+        } catch (MessagingException e){
+            throw new RuntimeException("Unable to send email", e);
+        }
+    }
+
+    public String generateRandomCode(int length){ //랜덤코드 생성
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijkLmnopqrstuvwxyz0123456789";
+        StringBuilder randomCode = new StringBuilder();
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+
+        for(int i = 0; i < length; i++){
+            int index = random.nextInt(characters.length());
+            randomCode.append(characters.charAt(index));
+        }
+        return randomCode.toString();
+    }
+
+    public boolean verifyCode(String email, String code) {
+        VerificationCode stored = codeMap.get(email);
+
+        if (stored == null || !stored.getCode().equals(code)) return false;
+        if (stored.getExpiresTime().isBefore(LocalDateTime.now())) return false;
+        codeMap.remove(email); // 인증 성공 시 삭제
+
+        return true;
+    }
+}
