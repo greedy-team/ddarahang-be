7dff026acbb7196fab42294b0db17802d88e6bf9
haeyoon1 <hynam1220@naver.com>
2025-05-06 01:59:07 +0900

    feat: EmailConfig 작성

---
+++ src/main/java/org/greedy/ddarahang/common/config/EmailConfig.java
@@ -0,0 +1,56 @@
+package org.greedy.ddarahang.common.config;
+
+import org.greedy.ddarahang.api.service.auth.MailService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfig {
+
+    @Value("${spring.mail.host}")
+    private String host;
+    @Value("${spring.mail.port}")
+    private int port;
+    @Value("${spring.mail.username}")
+    private String username;
+    @Value("${spring.mail.password}")
+    private String password;
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private boolean auth;
+    @Value("${spring.mail.properties.mail.smtp.timeout}")
+    private int timeout;
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private boolean starttls;
+
+    @Bean
+    public MailService mailService() {
+        return new MailService(javaMailSender());
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+        mailSender.setDefaultEncoding("UTF-8");
+        mailSender.setJavaMailProperties(getMailProperties());
+
+        return mailSender;
+    }
+
+    private Properties getMailProperties() {
+        Properties mailProperties = new Properties();
+        mailProperties.put("mail.smtp.auth", auth);
+        mailProperties.put("mail.smtp.timeout", timeout);
+        mailProperties.put("mail.smtp.starttls.enable", starttls);
+
+        return mailProperties;
+    }
+}
