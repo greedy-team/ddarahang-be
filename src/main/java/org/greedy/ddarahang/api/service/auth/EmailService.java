981250293bc4b75b14f3c72a086a310bb0275081
haeyoon1 <hynam1220@naver.com>
2025-05-11 16:28:33 +0900

    feat: 상태코드 변경

--- src/main/java/org/greedy/ddarahang/api/service/auth/EmailService.java
+++ src/main/java/org/greedy/ddarahang/api/service/auth/EmailService.java
@@ -20,6 +20,10 @@ public class EmailService {
 
     private final EmailSender emailSender;
 
+    private static final String RANDOM_CODE_CHARACTERS = "ABCDEFGHJKLMNPQRSTUVWXYZ" +
+            "abcdefghijkmnpqrstuvwxyz" +
+            "123456789";
+
     private final Map<String, VerificationCode> codeMap = new ConcurrentHashMap<>();
 
     public void sendCodeToEmail(String email) { //이메일 전송
@@ -44,13 +48,12 @@ public class EmailService {
     }
 
     public String generateRandomCode(int length){ //랜덤코드 생성
-        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijkLmnopqrstuvwxyz0123456789";
         StringBuilder randomCode = new StringBuilder();
         ThreadLocalRandom random = ThreadLocalRandom.current();
 
         for(int i = 0; i < length; i++){
-            int index = random.nextInt(characters.length());
-            randomCode.append(characters.charAt(index));
+            int index = random.nextInt(RANDOM_CODE_CHARACTERS.length());
+            randomCode.append(RANDOM_CODE_CHARACTERS.charAt(index));
         }
         return randomCode.toString();
     }
@@ -59,12 +62,12 @@ public class EmailService {
         VerificationCode stored = codeMap.get(email);
 
         if (stored == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body("인증 코드가 발송되지 않았습니다.");
         }
 
         if (!stored.getCode().equals(code)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body("인증 코드가 일치하지 않습니다.");
         }
 
