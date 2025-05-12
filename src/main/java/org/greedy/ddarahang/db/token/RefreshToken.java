9f72edd979f2b15b9dcb65fe327f059a7ae1e7f5
haeyoon1 <hynam1220@naver.com>
2025-05-12 17:22:51 +0900

    refactor: refreshtoken - user onetoone으로 변경 - 단일 기기 지원

--- src/main/java/org/greedy/ddarahang/db/token/RefreshToken.java
+++ src/main/java/org/greedy/ddarahang/db/token/RefreshToken.java
@@ -2,11 +2,13 @@ package org.greedy.ddarahang.db.token;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,7 +29,7 @@ public class RefreshToken {
 
     private String token;
 
-    @ManyToOne
+    @OneToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User user;
 
