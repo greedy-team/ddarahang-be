0a707ed47daf8de5fb626535718f89a33c122ea6
haeyoon1 <hynam1220@naver.com>
2025-05-27 15:15:40 +0900

    feat: 찜기능 관련 예외 파일 삭제

--- src/main/java/org/greedy/ddarahang/common/exception/NotFoundPlaceException.java
+++
@@ -1,7 +0,0 @@
-package org.greedy.ddarahang.common.exception;
-
-public class NotFoundPlaceException extends DdarahangException {
-    public NotFoundPlaceException(String message) {
-        super(message);
-    }
-}
