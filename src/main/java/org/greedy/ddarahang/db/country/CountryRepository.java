9cd48160f45d0ad4f3b3e3ae57d2194e25b7e92e
haeyoon1 <hynam1220@naver.com>
2025-07-12 15:03:32 +0900

    feat: 데이터 삽입을 위한 repository 메서드 추가

--- src/main/java/org/greedy/ddarahang/db/country/CountryRepository.java
+++ src/main/java/org/greedy/ddarahang/db/country/CountryRepository.java
@@ -3,6 +3,9 @@ package org.greedy.ddarahang.db.country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Long> {
+    Optional<Country> findByName(String name); // 이름으로 Country 조회 메서드 추가
 }
--- src/main/java/org/greedy/ddarahang/db/region/RegionRepository.java
+++ src/main/java/org/greedy/ddarahang/db/region/RegionRepository.java
@@ -3,6 +3,9 @@ package org.greedy.ddarahang.db.region;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Long> {
+    Optional<Region> findByName(String name); // 이름으로 Region 조회 메서드 추가
 }
