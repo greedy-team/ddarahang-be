506279424bfe957a442dd927f24f183aa11b69a0
haeyoon1 <hynam1220@naver.com>
2025-08-03 22:34:17 +0900

    refactor: TravelcourseController 로직 변경 - 내부에서 name -> id 매핑

--- src/main/java/org/greedy/ddarahang/api/controller/TravelCourseController.java
+++ src/main/java/org/greedy/ddarahang/api/controller/TravelCourseController.java
@@ -3,10 +3,13 @@ package org.greedy.ddarahang.api.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.greedy.ddarahang.api.dto.TravelCourseListIdRequest;
 import org.greedy.ddarahang.api.dto.TravelCourseListRequest;
 import org.greedy.ddarahang.api.dto.TravelCourseResponse;
 import org.greedy.ddarahang.api.dto.TravelCourseListResponse;
 import org.greedy.ddarahang.api.service.TravelCourseService;
+import org.greedy.ddarahang.db.country.CountryRepository;
+import org.greedy.ddarahang.db.region.RegionRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,24 +21,43 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/travelcourses")
 public class TravelCourseController {
 
     private final TravelCourseService travelCourseService;
+    private final CountryRepository countryRepository;
+    private final RegionRepository regionRepository;
 
-    @GetMapping
-    public ResponseEntity<Page<TravelCourseListResponse>> getTravelCourses(
-            @Valid @ModelAttribute TravelCourseListRequest request
-    ) {
+    @GetMapping("/api/v1/travelcourses")
+    public ResponseEntity<Page<TravelCourseListResponse>> getTravelCourses( @Valid @ModelAttribute TravelCourseListRequest request) {
         log.info("GET /travelcourses - sortField: {}, countryName: {}, regionName: {}, pageNumber: {}",
                 request.sortField(),
-                request.countryId(),
-                request.regionId(),
+                request.countryName(),
+                request.regionName(),
                 request.pageNumber());
-        return ResponseEntity.ok(travelCourseService.getTravelCourses(request));
+
+        Long countryId = null;
+        Long regionId = null;
+
+        if (request.regionName() != null && !request.regionName().isBlank()) {
+            regionId = regionRepository.findIdByName(request.regionName())
+                    .orElseThrow(() -> new IllegalArgumentException("Invalid region name"));
+        }
+        else {
+            countryId = countryRepository.findIdByName(request.countryName())
+                    .orElseThrow(() -> new IllegalArgumentException("Invalid country name"));
+        }
+
+        TravelCourseListIdRequest travelCourseListIdRequest = new TravelCourseListIdRequest(
+                countryId,
+                regionId,
+                request.pageNumber(),
+                request.sortField()
+        );
+
+        return ResponseEntity.ok(travelCourseService.getTravelCourses(travelCourseListIdRequest));
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/api/v1/travelcourses/{id}")
     public ResponseEntity<TravelCourseResponse> getTravelCourseDetail(@PathVariable Long id) {
         log.info("GET /travelcourses/{}", id);
         return ResponseEntity.ok(travelCourseService.getTravelCourseDetail(id));
