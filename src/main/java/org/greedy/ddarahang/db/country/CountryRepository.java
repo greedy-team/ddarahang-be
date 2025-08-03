b881ca35952a8dc793ea9da6300752dea5f6c5fc
haeyoon1 <hynam1220@naver.com>
2025-08-03 22:31:31 +0900

    feat: country, region Repository에서 name으로 id찾는 메서드 추가

--- src/main/java/org/greedy/ddarahang/db/country/CountryRepository.java
+++ src/main/java/org/greedy/ddarahang/db/country/CountryRepository.java
@@ -1,6 +1,9 @@
 package org.greedy.ddarahang.db.country;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -8,4 +11,7 @@ import java.util.Optional;
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Long> {
     Optional<Country> findByName(String name); // 이름으로 Country 조회 메서드 추가
+
+    @Query("SELECT c.id FROM Country c WHERE c.name = :name")
+    Optional<Long> findIdByName(@Param("name") String name);
 }
--- src/main/java/org/greedy/ddarahang/db/region/RegionRepository.java
+++ src/main/java/org/greedy/ddarahang/db/region/RegionRepository.java
@@ -1,6 +1,8 @@
 package org.greedy.ddarahang.db.region;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -8,4 +10,7 @@ import java.util.Optional;
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Long> {
     Optional<Region> findByName(String name); // 이름으로 Region 조회 메서드 추가
+
+    @Query("SELECT r.id FROM Region r WHERE r.name = :name")
+    Optional<Long> findIdByName(@Param("name") String name);
 }
